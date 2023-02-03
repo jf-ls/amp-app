3bc2f32949acd446f0759c8f85144ab8c4f2bb4c
jf-ls <james.forbes@lightstream.com>
2023-02-02 23:38:02 -0800

    try again...

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <div>HAHAHA YES I OWN YOU</div>
+          <div>HAHAHA YES I AM THE NEW KING OF AWS BOW BEFORE MY POWER</div>
         </p>
         <a
           className="App-link"
