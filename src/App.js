fbbccf39c1301c00131a263f14877f4e4e6bf882
jf-ls <james.forbes@lightstream.com>
2023-02-02 23:29:35 -0800

    try again...

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.\n
+          Edit <code>src/App.js</code> and save to reload.<br>
           HAHAHA YES I OWN YOU
         </p>
         <a
